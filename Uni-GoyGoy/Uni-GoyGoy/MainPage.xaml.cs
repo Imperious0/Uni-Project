343d52d563d3152f473593a09548c0e9e434e7de
Alp <alparslan226@gmail.com>
2018-11-22 21:21:25 +0300

    Update MainPage.xaml.cs

--- Uni-GoyGoy/Uni-GoyGoy/MainPage.xaml.cs
+++ Uni-GoyGoy/Uni-GoyGoy/MainPage.xaml.cs
@@ -9,6 +9,7 @@ namespace Uni_GoyGoy
 {
     public partial class MainPage : ContentPage
     {
+        Button[] btns = new Button[4];
         public MainPage()
         {
 
@@ -22,39 +23,37 @@ namespace Uni_GoyGoy
             rl_btns.HeightRequest = 100;
             
 
-            Button[] btns = new Button[4];
+
             for (int i = 0; i < btns.GetLength(0); i++)
             {
-                btns[i] = new Button();
-                btns[i].Text = "Holy Fuck İt Works!";
-                btns[i].BackgroundColor = Color.Green;
-                btns[i].TextColor = Color.Goldenrod;
-                btns[i].WidthRequest = 100;
-                btns[i].HeightRequest = 100;
+                btns[i] = new Button
+                {
+                    Text = "Holy Fuck İt Works! " + i,
+                    BackgroundColor = Color.Green,
+                    TextColor = Color.Goldenrod,
+                    WidthRequest = 80,
+                    HeightRequest = 100
+                };
             }
+
             int button_pad = 10;
-            int buttons_width = ((int)this.Width - (btns.GetLength(0)* button_pad)) / btns.GetLength(0);
-            
-            Console.WriteLine("btn: "+buttons_width + " rl: " + rl.Width);
+
             for (int i = 0; i < btns.GetLength(0); i++)
             {
-                Console.WriteLine("i1: " + i + "\n");
+                int c = (i * (int)btns[i].WidthRequest);
+                int v = i * button_pad;
                 rl_btns.Children.Add(btns[i],
-               /* Index Out Of Bound!*/ Constraint.RelativeToParent((x) => { return x.Width - ((i+1) * btns[i].Width) - (i * button_pad); }),
+                Constraint.RelativeToParent((x) => { return c+v; }),
                 Constraint.RelativeToParent((x) => { return 0; }));
-                Console.WriteLine("i2: " + i +"\n");
             }
 
 
 
 
-                rl.Children.Add(rl_btns,
-        Constraint.RelativeToParent((x) => { return 0; }),
-        Constraint.RelativeToParent((x) => { return x.Height - 100; }));
+            rl.Children.Add(rl_btns,
+            Constraint.RelativeToParent((x) => { return 0; }),
+            Constraint.RelativeToParent((x) => { return x.Height - 100; }));
 
-            
-
-            Console.WriteLine("rl next: " + rl.WidthRequest + " rl.wd: " + rl.Width + " rl_btn: " + rl_btns.Width + " rl btns_w: " + rl_btns.WidthRequest);
 
             this.Content = rl;
         }
