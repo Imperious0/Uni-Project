1927fa57814406090ccd26db18e983aef56261a4
Alp <alparslan226@gmail.com>
2018-11-22 14:34:04 +0300

    a

---
+++ Uni-GoyGoy/Uni-GoyGoy/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Uni_GoyGoy
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+
+            
+
+            InitializeComponent();
+
+            this.BackgroundColor = Color.White;
+            RelativeLayout rl = new RelativeLayout();
+            Button bt = new Button();
+            bt.Text = "Holy Fuck İt Works!";
+            bt.BackgroundColor = Color.Green;
+            bt.TextColor = Color.Goldenrod;
+            bt.WidthRequest = 100;
+            bt.HeightRequest = 100;
+            rl.Children.Add(bt,
+                    Constraint.RelativeToParent((x) => { return (x.Width / 2) - 50; }),
+                    Constraint.RelativeToParent((x) => { return (x.Height / 2) - 50; }));
+            this.Content = rl;
+
+        }
+    }
+}
